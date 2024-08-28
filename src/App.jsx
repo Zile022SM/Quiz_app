541800f9b5643e4467a0446b0280bf594f79aafd
Zile022SM <zile.kuzminac85@gmail.com>
2024-08-28 22:36:20 +0200

    Quiz question loaded via useReducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
 import Main from './assets/Main'
+import { useEffect, useReducer } from 'react'
+
+const initialState={
+  questions:[],
+
+  //loading,error,ready,active,finished
+  status:'loading',
+}
+
+function reducer(state,action){
+  switch(action.type){
+    case 'dataRecived':
+      return {...state,questions:action.payload,status:'ready'};
+
+    case 'dataFailed':
+        return {...state,status:'error'};
+
+    default: throw new Error('Action unknown');
+  }
+}
 
 function App() {
+
+  const [state,dispatch]=useReducer(reducer,initialState);
+
+  useEffect(function(){
+      fetch("http://localhost:9000/questions").then((res)=>res.json()).then((data)=>dispatch({type:'dataRecived',payload:data})).catch((err)=>dispatch({type:'dataFailed'}));
+  },[])
 
   return (
     <div className='app'>
