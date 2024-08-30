707799fa9e4156a7372071541ea50fcc22ab01b7
Zile022SM <zile.kuzminac85@gmail.com>
2024-08-30 19:10:34 +0200

    Handling New Answers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer:null,
-
+  points:0
 };
 
 function reducer(state, action) {
@@ -27,14 +27,15 @@ function reducer(state, action) {
     case "active":
       return { ...state, status: "active" };
     case "newAnswer":
-      return { ...state, answer: action.payload };
+      const question = state.questions.at(state.index);
+      return { ...state, answer: action.payload,points:action.payload === question.correctOption ? state.points + question.points : state.points};
     default:
       throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [{ questions, status, index,answer }, dispatch] = useReducer(
+  const [{ questions, status, index,answer,points }, dispatch] = useReducer(
     reducer,
     initialState
   );
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function Options({ options, dispatch,answer,question }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
         {options.map((option, index) => (
-          <button key={index} className={`btn btn-option ${index === answer ? 'answer' : ''} ${index === question.correctOption ? "correct" : "wrong"}`} onClick={()=>dispatch({type:'newAnswer',payload:index})} disabled={answer !== null} >{option}</button>
+          <button key={index} className={`btn btn-option ${index === answer ? 'answer' : ''} 
+          
+            ${hasAnswered ? index === question.correctOption ? "correct" : "wrong":""}`}
+            
+            onClick={()=>dispatch({type:'newAnswer',payload:index})} disabled={hasAnswered}
+
+          >{option}</button>
         ))}
       </div>
   );
